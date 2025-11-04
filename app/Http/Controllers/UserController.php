cc30792d247db6010eac312128e70746a98caa17
Pasith Senevirathna <pasith.info@gmail.com>
2025-11-04 18:24:53 +0530

    Fix error handling in user store method to return error log message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
 
         } catch (\Exception $e) {
             // throw $th;
-            error_log($e->getMessage());
+            return error_log($e->getMessage());
         }
 
         return redirect()->route('users.index')
