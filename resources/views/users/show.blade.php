b8cbd90423167b2e452dfa2f7dad1a5c9f60afbd
Pasith Senevirathna <pasith.info@gmail.com>
2025-11-04 19:08:03 +0530

    Refactor user details view for improved structure and readability

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+@extends('users.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Show User Details</h2>
+        </div>
+        <div class="pull-right">
+            <a href="{{ route('users.index') }}" class="btn btn-primary">Back</a>
+        </div>
+    </div>
 </div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {{ $user->name }}
+        </div>
+<div class="form-group">
+    <strong>Email:</strong>
+    {{ $user->email }}
+</div>
+    </div>
+</div>
+@endsection
